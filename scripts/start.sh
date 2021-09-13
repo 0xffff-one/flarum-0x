d66bcbabaf1805480e84a88781284ad20fcfed82
zgq354 <zgq354@gmail.com>
2021-09-14 00:50:32 +0800

    chore: mkdir and chown at startup

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,27 @@
 set -e
 role=${CONTAINER_ROLE:-app}
 
+mkfile() { mkdir -p "$(dirname "$1")" && touch "$1" ;  }
+
 if [ "$role" = "app" ]; then
+    # storage
+    mkfile /wwwroot/storage/cache/.gitignore
+    mkfile /wwwroot/storage/formatter/.gitignore
+    mkfile /wwwroot/storage/less/.gitignore
+    mkfile /wwwroot/storage/locale/.gitignore
+    mkfile /wwwroot/storage/logs/.gitignore
+    mkfile /wwwroot/storage/sessions/.gitignore
+    mkfile /wwwroot/storage/tmp/.gitignore
+    mkfile /wwwroot/storage/views/.gitignore
+
+    # assets
+    mkfile /wwwroot/public/assets/avatars/.gitignore
+
+    # permissions
+    chown -R www-data:www-data /wwwroot/storage
+    chown -R www-data:www-data /wwwroot/public/assets
+
+    # start
     exec php-fpm
 elif [ "$role" = "queue" ]; then
     # flarum queue
