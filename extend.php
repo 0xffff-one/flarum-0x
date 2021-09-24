996c5d7bf4b8bddb347bbc1eb8f74ec20ca3a400
zgq354 <zgq354@gmail.com>
2021-09-24 09:26:36 +0800

    feat: throttle filter for dumplicate posts #28

--- extend.php
+++ extend.php
@@ -9,7 +9,12 @@
 
 use Flarum\Extend;
 use Flarum\Discussion\Event\Saving;
+use Flarum\Extend\ThrottleApi;
+use Flarum\Http\RequestUtil;
+use Flarum\Post\Post;
+use Illuminate\Support\Arr;
 use Overtrue\Pinyin\Pinyin;
+use Psr\Http\Message\ServerRequestInterface;
 
 // https://github.com/overtrue/pinyin
 $pinyin = new Pinyin();
@@ -27,5 +32,26 @@ return [
         'password' => null,
         'port' => 6379,
         'database' => 1
-    ])
+    ]),
+    // check dumplicate post in the last 10 minutes
+    (new ThrottleApi())
+        ->set('checkDumplicate', function (ServerRequestInterface $request) {
+            if (!in_array($request->getAttribute('routeName'), ['discussions.create', 'posts.create'])) {
+                return;
+            }
+
+            $actor = RequestUtil::getActor($request);
+
+            $latestPost = Post::where('user_id', $actor->id)
+                ->where('created_at', '>=', new DateTime('-10 minutes'))
+                ->orderBy('id', 'DESC')
+                ->first();
+            if ($latestPost) {
+                $latestContent = resolve('flarum.formatter')->unparse($latestPost->content);
+                $currentContent = Arr::get($request->getParsedBody(), 'data.attributes.content', '');
+                if ($latestContent === $currentContent) {
+                    return true;
+                }
+            }
+        }),
 ];
