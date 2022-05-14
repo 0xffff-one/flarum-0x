6e231db506bd711cc8dcda4d6da6b381a6c41279
zgq354 <zgq354@gmail.com>
2022-05-15 00:29:39 +0800

    feat: use image built by GitHub Action

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-cd $SCRIPTPATH/.. \
+sudo docker pull zgq354/0xffff-flarum:latest \
+    && cd $SCRIPTPATH/.. \
     && sudo docker-compose up -d --build --remove-orphans \
     && sudo docker exec -it 0xffff-app php flarum cache:clear
